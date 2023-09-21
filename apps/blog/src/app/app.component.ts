7a03dc7a93ac528ff3afe1ac45b28608e7fb4dbf
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-09-21 17:07:13 +0700

    add button lib

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonComponent } from '@nx-app/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
   selector: 'nx-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
