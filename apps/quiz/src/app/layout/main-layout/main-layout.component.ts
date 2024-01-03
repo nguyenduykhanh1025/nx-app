49c89ac3cf545ff9df7f279b8acf75426635b77f
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2024-01-03 17:05:24 +0700

    add footer

--- apps/quiz/src/app/layout/main-layout/main-layout.component.ts
+++ apps/quiz/src/app/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ButtonIconComponent, FlashCardComponent, IconComponent } from '@nx-app/web/libs';
+import { ButtonComponent, ButtonIconComponent, FlashCardComponent, IconComponent } from '@nx-app/web/libs';
 
 @Component({
   selector: 'quiz-main-layout',
   standalone: true,
-  imports: [CommonModule, IconComponent, ButtonIconComponent, FlashCardComponent],
+  imports: [CommonModule, IconComponent, ButtonIconComponent, FlashCardComponent, ButtonComponent],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
