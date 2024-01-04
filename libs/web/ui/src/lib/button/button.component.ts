e6b35fb56dcbac0224808e7fb9fe7cfcce7d8fe7
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2024-01-04 13:34:29 +0700

    add snipper component

--- libs/web/ui/src/lib/button/button.component.ts
+++ libs/web/ui/src/lib/button/button.component.ts
@@ -1,11 +1,29 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgIcon } from '../model/svg-icon';
+import { ButtonSize } from '../model/button-size';
+import { IconComponent } from '../icon/icon.component';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { SnipperSize } from '../model/snipper-size';
 
 @Component({
   selector: 'ui-button',
   standalone: true,
   encapsulation: ViewEncapsulation.None,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent, SpinnerComponent],
   templateUrl: './button.component.html',
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() svgIcon: SvgIcon = '';
+  @Input() isLoading = false;
+  @Input() isDisabled = false;
+  @Input() size = ButtonSize.MEDIUM;
+
+  @Output() uiClick = new EventEmitter<void>();
+
+  readonly SnipperSize = SnipperSize;
+
+  get isShowIcon(): boolean {
+    return !!this.svgIcon && !this.isLoading;
+  }
+}
---
+++ libs/web/ui/src/lib/model/snipper-size.ts
@@ -0,0 +1,5 @@
+export enum SnipperSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large'
+}
